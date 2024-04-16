1df9c1f61ebfdd8a1155f223f50e35251eee7008
min khant <minkhant4287@gmail.com>
2024-04-16 16:09:16 +0630

    News App

---
+++ app.js
@@ -0,0 +1,87 @@
+// *7db529c036c64784aa8b78104c8829e3
+// *http://newsapi.org/s/u
+
+//API Used: http://newsapi.org/s/india-news-api
+const container = document.querySelector(".container");
+const optionsContainer = document.querySelector(".options-container");
+
+const country = "us";
+const options = [
+    "general",
+    "entertainment",
+    "health",
+    "science",
+    "sports",
+    "entertainment",
+    "technology",
+];
+
+//100 requests per day
+let requestURL;
+let apiKey;
+
+//Create cards from data
+const generateUI = (articles) => {
+    for (let item of articles) {
+        let card = document.createElement("div");
+        card.classList.add("news-card");
+        card.innerHTML = `<div class="news-image-container">
+    <img src="${item.urlToImage || "./newspaper.jpg"}" alt="news-img" />
+    </div>
+    <div class="news-content">
+      <div class="news-title">
+        ${item.title}
+      </div>
+      <div class="news-description">
+      ${item.description || item.content || ""}
+      </div>
+      <a href="${item.url}" target="_blank" class="view-button">Read More</a>
+    </div>`;
+        container.appendChild(card);
+    }
+};
+
+//News API Call
+const getNews = async() => {
+    container.innerHTML = "";
+    let response = await fetch(requestURL);
+    if (!response.ok) {
+        alert("Data unavailable at the moment. Please try again later");
+        return false;
+    }
+    let data = await response.json();
+    generateUI(data.articles);
+};
+
+//Category Selection
+const selectCategory = (e, category) => {
+    let options = document.querySelectorAll(".option");
+    options.forEach((element) => {
+        element.classList.remove("active");
+    });
+    requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&language=&apiKey=${apiKey}`;
+    e.target.classList.add("active");
+    getNews();
+};
+
+//Options Buttons
+const createOptions = () => {
+    for (let i of options) {
+        optionsContainer.innerHTML += `<button class="option ${
+      i == "general" ? "active" : ""
+    }" onclick="selectCategory(event,'${i}')">${i}</button>`;
+    }
+};
+
+const init = () => {
+    optionsContainer.innerHTML = "";
+    getNews();
+    createOptions();
+};
+
+window.onload = () => {
+
+    apiKey = "7db529c036c64784aa8b78104c8829e3";
+    requestURL = `https://newsapi.org/v2/top-headlines?country=${country}&category=general&apiKey=${apiKey}`;
+    init();
+};
